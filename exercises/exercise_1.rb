22cf9e80a101c27b52d41cdebe8dcf2d11993160
Rachel Gould <rachegould@gmail.com>
2019-03-19 21:18:53 +0000

    ex1 done

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,32 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+# def initialize(name, annual_revenue, mens_apparel, womens_apparel)
+# @name = name
+# @annual_revenue = annual_revenue
+# @mens_apparel = mens_apparel
+# @womens_apparel = womens_apparel
+# end
+
+burnaby = Store.create(
+  name: "Burnaby", 
+  annual_revenue: 300000, 
+  mens_apparel: true, 
+  womens_apparel: true
+  )
+
+richmond = Store.create(
+  name: "Richmond", 
+  annual_revenue: 1260000, 
+  mens_apparel: false, 
+  womens_apparel: true
+  )
+
+gastown = Store.create(
+  name: "Gastown", 
+  annual_revenue: 190000, 
+  mens_apparel: true, 
+  womens_apparel: false
+  )
+
+puts Store.all.count
