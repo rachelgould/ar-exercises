24e353ad28686ad09578738ed321ce57548bee58
Rachel Gould <rachegould@gmail.com>
2019-03-19 21:46:25 +0000

    ex5 complete

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,12 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+@total_revenue = Store.sum("annual_revenue")
+puts "Total Revenue: ", @total_revenue
+
+@avg_revenue = @total_revenue / Store.all.count
+puts "Average Revenue: ", @avg_revenue
+
+@high_earning_stores = Store.where(annual_revenue: 1000000...Float::INFINITY).count
+
+puts "Number of stores making > $1M: ", @high_earning_stores
